0db75100ca24da10db378bf49eeb597f933f94d3
Takashi Oda <takashioda@mbp.local>
2020-07-22 14:25:21 +0900

    action_mailerのGmail設定をproduction.rbにも記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "trima_v0_0_production"
 
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'trima-staging2.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'cedarsdev01@gmail.com',
+    :password => 'Cedars4112',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
