997b963e8307120f13d5e384fa9945109af8cc95
Takashi Oda <takashioda@mbp.local>
2020-07-27 10:56:37 +0900

    uploader.rbのstrage記述が逆だったのを修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,28 +4,16 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # S3にアップロードする場合
-  if Rails.env.production? || Rails.env.test?
-    storage :file
-  else
+  if Rails.env.production?
     storage :fog
+  else
+    storage :file
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
 
   # デフォルト画像は700x700に収まるようリサイズ
   process :resize_to_fill => [300, 300, gravity = ::Magick::CenterGravity]
@@ -49,7 +37,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # 保存するファイルの命名規則
   def filename
-     "something.jpg" if original_filename
+     "user_avatar.jpg" if original_filename
   end
 
   protected
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,23 +2,24 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = ENV['S3_BUCKET']
-    # config.asset_host = 'https://s3.amazonaws.com/trima-images'
-    config.asset_host = 'https://trima-images.s3.amazonaws.com' #このように修正
+    config.asset_host = 'https://s3.amazonaws.com/trima-images'
+    # config.asset_host = 'https://trima-images.s3.amazonaws.com' #このように修正
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region: ENV['S3_REGION']
+      region: 'ap-southeast-1',
+      path_style: true
     }
-    # キャッシュをS3に保存
-    # config.cache_storage = :fog
   else
     config.storage :file
-    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+    # config.enable_processing = false if Rails.env.test? #test:処理をスキップ
   end
 end
