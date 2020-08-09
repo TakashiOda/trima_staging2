d2f30c1470e0a5a33817d0eb7bba48ddb6dce9f5
Takashi Oda <wiwi5223@gmail.com>
2020-08-09 14:43:49 +0900

    productionのategory_seed追加

---
+++ db/fixtures/production/07_activity_category_list.rb
@@ -0,0 +1,8 @@
+require 'csv'
+csv = CSV.read('db/fixtures/production/activity_categories.csv')
+csv.each do |data|
+  ActivityCategory.seed(:en_name) do |model|
+    model.en_name = data[0]
+    model.jp_name = data[1]
+  end
+end
