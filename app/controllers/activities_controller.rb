a952d3b6000a1f14bfc9610cc6c04a728e30e14b
TakashiOda <wiwi5223@gmail.com>
2020-09-12 19:17:19 +0900

    在庫を月毎に出し分ける画面newだけ作成

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -51,20 +51,202 @@ class ActivitiesController < ApplicationController
     if !@courses.blank?
       if @activity.is_all_year_open
         #通年の体験
-        @s_Date = Date.tomorrow
-        @e_Date = Date.tomorrow.since(3.month).end_of_month
+        #当月分の在庫 new
+        @s_Date = Date.tomorrow #体験の開始日
+        @this_month_end = Date.tomorrow.end_of_month #当月の末日
+        # @e_Date = Date.tomorrow.since(3.month).end_of_month
         @courses.each do |course|
-          (@s_Date..@e_Date).each do |date|
-            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          (@s_Date..@this_month_end).each do |date|
+            @this_month_stocks = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
           end
         end
-        
+
+        #来月分の在庫 new
+        @next_month_first = @this_month_end + 1 #来月1日
+        @next_month_end = @next_month_first.end_of_month #当月の末日
+        # @e_Date = Date.tomorrow.since(3.month).end_of_month
+        @courses.each do |course|
+          (@next_month_first..@next_month_end).each do |date|
+            @next_month_stocks = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+
+        #2月後分の在庫 new
+        @next2_month_first = @next_month_end + 1 #2ヶ月後の1日
+        @next2_month_end = @next2_month_first.end_of_month #当月の末日
+        # @e_Date = Date.tomorrow.since(3.month).end_of_month
+        @courses.each do |course|
+          (@next2_month_first..@next2_month_end).each do |date|
+            @next2_month_stocks = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+
+        #3月後分の在庫 new
+        @next3_month_first = @next2_month_end + 1 #2ヶ月後の1日
+        @next3_month_end = @next3_month_first.end_of_month #当月の末日
+        # @e_Date = Date.tomorrow.since(3.month).end_of_month
+        @courses.each do |course|
+          (@next3_month_first..@next3_month_end).each do |date|
+            @next3_month_stocks = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+
       elsif !@activity.is_all_year_open && @activity.start_date && @activity.start_date
         #期間限定の体験
         @s_Date = @activity.start_date
+        @this_month_end = @activity.start_date.end_of_month
         @e_Date = @activity.end_date
         @courses.each do |course|
-          (@s_Date..@e_Date).each do |date|
+          (@s_Date..@this_month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+      else
+        flash[:alert] = '在庫を設定するには体験情報にて期間設定をしてください'
+        redirect_to supplier_activities_path(current_supplier)
+      end
+    else
+      flash[:alert] = '在庫を設定するには体験情報のコース時間または期間設定をしてください'
+      redirect_to supplier_activities_path(current_supplier)
+    end
+  end
+
+  def stock_new_first_month
+    # binding.pry
+    @activity = Activity.find(params[:activity_id])
+    @courses = @activity.activity_courses
+    if !@courses.blank?
+      if @activity.is_all_year_open
+        #通年の体験
+        #当月分の在庫 new
+        @s_Date = Date.tomorrow #体験の開始日
+        @month_end = Date.tomorrow.end_of_month #当月の末日
+        # @e_Date = Date.tomorrow.since(3.month).end_of_month
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+
+      elsif !@activity.is_all_year_open && @activity.start_date && @activity.start_date
+        #期間限定の体験
+        @s_Date = @activity.start_date
+        @month_end = @activity.start_date.end_of_month
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+      else
+        flash[:alert] = '在庫を設定するには体験情報にて期間設定をしてください'
+        redirect_to supplier_activities_path(current_supplier)
+      end
+    else
+      flash[:alert] = '在庫を設定するには体験情報のコース時間または期間設定をしてください'
+      redirect_to supplier_activities_path(current_supplier)
+    end
+  end
+
+  def stock_new_next_month
+    # binding.pry
+    @activity = Activity.find(params[:activity_id])
+    @courses = @activity.activity_courses
+    if !@courses.blank?
+      if @activity.is_all_year_open
+        #通年の体験
+        #当月分の在庫 new
+        @s_Date = Date.tomorrow.end_of_month + 1 #翌月1日
+        @month_end = @s_Date.end_of_month #翌月末
+        # @e_Date = Date.tomorrow.since(3.month).end_of_month
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+
+      elsif !@activity.is_all_year_open && @activity.start_date && @activity.start_date
+        #期間限定の体験
+        @s_Date = @activity.start_date.end_of_month + 1
+        @month_end = @s_Date.end_of_month
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+      else
+        flash[:alert] = '在庫を設定するには体験情報にて期間設定をしてください'
+        redirect_to supplier_activities_path(current_supplier)
+      end
+    else
+      flash[:alert] = '在庫を設定するには体験情報のコース時間または期間設定をしてください'
+      redirect_to supplier_activities_path(current_supplier)
+    end
+  end
+
+  def stock_new_next2_month
+    @activity = Activity.find(params[:activity_id])
+    @courses = @activity.activity_courses
+    if !@courses.blank?
+      if @activity.is_all_year_open
+        #通年の体験
+        #当月分の在庫 new
+        @s_Date = (Date.tomorrow >> 1).end_of_month + 1 #翌月1日
+        @month_end = @s_Date.end_of_month #翌月末
+        # @e_Date = Date.tomorrow.since(3.month).end_of_month
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+
+      elsif !@activity.is_all_year_open && @activity.start_date && @activity.start_date
+        #期間限定の体験
+        @s_Date = (@activity.start_date >> 1).end_of_month + 1
+        @month_end = @s_Date.end_of_month
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+      else
+        flash[:alert] = '在庫を設定するには体験情報にて期間設定をしてください'
+        redirect_to supplier_activities_path(current_supplier)
+      end
+    else
+      flash[:alert] = '在庫を設定するには体験情報のコース時間または期間設定をしてください'
+      redirect_to supplier_activities_path(current_supplier)
+    end
+  end
+
+  def stock_new_next3_month
+    @activity = Activity.find(params[:activity_id])
+    @courses = @activity.activity_courses
+    if !@courses.blank?
+      if @activity.is_all_year_open
+        #通年の体験
+        #当月分の在庫 new
+        @s_Date = (Date.tomorrow >> 2).end_of_month + 1 #翌月1日
+        @month_end = @s_Date.end_of_month #翌月末
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
+            @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+            # @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
+          end
+        end
+
+      elsif !@activity.is_all_year_open && @activity.start_date && @activity.start_date
+        #期間限定の体験
+        @s_Date = (@activity.start_date >> 2).end_of_month + 1
+        @month_end = @s_Date.end_of_month
+        @courses.each do |course|
+          (@s_Date..@month_end).each do |date|
             @stock = course.activity_stocks.build(date: date, stock: @activity.maximum_num)
           end
         end
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,10 @@ Rails.application.routes.draw do
 
     resources :activities do
       get "stocknew", to: "activities#stock_new", as: :new_stocks
+      get "stock_new_first_month", to: "activities#stock_new_first_month", as: :new_stocks_first_month
+      get "stock_new_next_month", to: "activities#stock_new_next_month", as: :new_stocks_next_month
+      get "stock_new_next2_month", to: "activities#stock_new_next2_month", as: :new_stocks_next2_month
+      get "stock_new_next3_month", to: "activities#stock_new_next3_month", as: :new_stocks_next3_month
       # get "suppliers/:supplier_id/activities/:activity_id/stocknew", to: "activities#stock_new", as: :new_stocks
       post "stocks", to: "activities#stock_create"
       get "stockedit", to: "activities#stock_edit"
