6d8f3daafb4237e2421b20a9c197f2e0483ae3cb
TakashiOda <wiwi5223@gmail.com>
2020-10-21 12:26:50 +0900

    pagination追加

--- app/controllers/basic_datas_controller.rb
+++ app/controllers/basic_datas_controller.rb
@@ -1,6 +1,8 @@
 class BasicDatasController < ApplicationController
   def index
-    @towns = Town.all
+    # @towns = Town.all
+    @towns = Town.where.not('jp_name like ?','%--%').page(params[:page]).per(20)
     # @towns = Town.where.not(town_code: "nil")
+    # モデル名.where.not('カラム名 like ?','%検索したい文字列%')
   end
 end
