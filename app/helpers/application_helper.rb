ed504699b2bce92c5b7b2fbbff9d0380c1b34bf0
TakashiOda <wiwi5223@gmail.com>
2020-11-19 10:22:47 +0900

    ogp設定&ogp画像設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'trima',
+      title: 'is a travel manage webapp',
+      reverse: true,
+      separator: '|',
+      description: 'travel manage tool',
+      keywords: 'trima',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'trima',
+        title: 'trima',
+        description: 'travel manage tool',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('trima_ogp_user_color.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@odayan3',
+      },
+      fb: {
+        app_id: 'takashi.oda.921'
+      }
+    }
+  end
 end
