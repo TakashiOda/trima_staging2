4db083b4ceabf743ccb9470b8819af6b9db770ef
TakashiOda <wiwi5223@gmail.com>
2020-12-20 16:27:14 +0900

    cedars.sakura.ne.jpni

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'cedars.jp',
+    domain: 'cedars.sakura.ne.jp',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
