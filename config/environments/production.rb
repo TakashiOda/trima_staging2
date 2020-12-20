c2595fb897b45af961dd713679daa1ec1218fe3d
TakashiOda <wiwi5223@gmail.com>
2020-12-20 17:46:09 +0900

    production設定からdomains削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,13 +68,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['PRODUCTION_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => ENV['DEV_MAILER_ADDRESS'],
-    :domain => ENV['DEV_MAILER_ADDRESS'],
+    :address => 'cedars.sakura.ne.jp',
     :port => 587,
     :user_name => ENV['DEV_MAILER_USER_NAME'],
     :password => ENV['DEV_MAILER_PASSWORD'],
-    :authentication => :login,
-    :enable_starttls_auto => true
+    :authentication => :login
+    # :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
