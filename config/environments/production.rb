a03311f3bc3f8b8f6425d5ecfd53b60bf157646b
TakashiOda <wiwi5223@gmail.com>
2020-12-21 11:11:21 +0900

    cedars.sakura.ne.jp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['PRODUCTION_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'cedars.jp',
-    domain:  'cedars.jp',
+    address: 'cedars.sakura.ne.jp',
+    domain:  'cedars.sakura.ne.jp',
     port:           587,
     user_name:      'uu-trima@cedars.jp',
     password:       'N1Ya*Wvi',
