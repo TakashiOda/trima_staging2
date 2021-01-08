c4574d3491ac99c6159206ac20f2158b509c07fd
TakashiOda <wiwi5223@gmail.com>
2021-01-08 10:33:38 +0900

    activity_bizのvalidation追加

--- app/models/activity_ageprice.rb
+++ app/models/activity_ageprice.rb
@@ -1,5 +1,12 @@
 class ActivityAgeprice < ApplicationRecord
-  belongs_to :activity
+  # relation ***********************************************
+  belongs_to :activity # activityの子である
 
-  validates :activity_id, uniqueness: { scope: :age_from }
+  # validation **********************************************
+  validates :activity_id, uniqueness: { scope: :age_from } # "activity_id"と "age_from"は複合ユニーク
+  validates :age_from, presence: true, numericality: { only_integer: true } # "age_from"は 空でない / 整数のみ
+  validates :age_to, presence: true, numericality: { only_integer: true } # "age_to"は 空でない / 整数のみ
+  validates :normal_price, presence: true, numericality: { only_integer: true } # "normal_price"は 空でない / 整数のみ
+  validates :high_price, presence: true, numericality: { only_integer: true } # "high_price"は 空でない / 整数のみ
+  validates :low_price, presence: true, numericality: { only_integer: true } # "low_price"は 空でない / 整数のみ
 end
--- app/models/activity_business.rb
+++ app/models/activity_business.rb
@@ -1,4 +1,6 @@
 class ActivityBusiness < ApplicationRecord
+
+  # relation ***********************************************
   belongs_to :supplier
   has_many :activities, dependent: :destroy
   has_many :guides, dependent: :destroy
@@ -8,7 +10,33 @@ class ActivityBusiness < ApplicationRecord
   has_many :languages, through: :activity_languages
   accepts_nested_attributes_for :activity_languages
 
+  # uploader ***********************************************
   mount_uploader :profile_image, AvatarUploader
   mount_uploader :insurance_image, InsuranceImageUploader
+
+  # validation ***********************************************
+  validates :name, presence: true, length: { in: 2..50, message: "2文字以上50文字以下" }
+  validates :en_name, length: { in: 2..50, message: "2文字以上50文字以下" }, allow_blank: true
+  validates :cn_name, length: { in: 2..50, message: "2文字以上50文字以下" }, allow_blank: true
+  validates :tw_name, length: { in: 2..50, message: "2文字以上50文字以下" }, allow_blank: true
+  validates :profile_text, presence: true, length: { in: 5..300, message: "5文字以上300文字以下" }
+  validates :en_profile_text, length: { in: 5..300, message: "5文字以上300文字以下" }, allow_blank: true
+  validates :cn_profile_text, length: { in: 5..300, message: "5文字以上300文字以下" }, allow_blank: true
+  validates :tw_profile_text, length: { in: 5..300, message: "5文字以上300文字以下" }, allow_blank: true
+  validates :apply_suuplier_address, inclusion: { in: [true, false] }
+  validates :apply_suuplier_phone, inclusion: { in: [true, false] }
+
+  VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}\z/
+  validates :email, format: { with: VALID_POSTCODE_REGEX, message: "ハイフンあり半角数字のみ" }
+  validates :post_code, inclusion: { in: [true, false] }, allow_blank: true
+  validates :phone, inclusion: { in: [true, false] }, allow_blank: true
+
+  validates :detail_address, length: { maximum: 100, too_long: "最大%{count}文字まで" }, allow_blank: true
+  validates :building, length: { maximum: 100, too_long: "最大%{count}文字まで" }, allow_blank: true
+  validates :no_charge_cansel_before, inclusion: { in: %w(the_day_before three_days_before a_week_before) }
+  validates :status, inclusion: { in: %w(inputing send_approve) }
+  validates :guide_certification, length: { maximum: 100, too_long: "最大%{count}文字まで" }, allow_blank: true
+  validates :has_insurance, inclusion: { in: [true, false] }
+  validates :is_approved, inclusion: { in: [true, false] }
 
 end
