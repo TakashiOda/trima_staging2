cf661ee02571b29a7c8b6be6cd698ffe5698813a
TakashiOda <wiwi5223@gmail.com>
2021-02-01 12:50:15 +0900

    form修正

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -15,26 +15,30 @@ class SuppliersController < ApplicationController
     @supplier = current_supplier
     @supplier_profile = @supplier.supplier_profile
     @activity_business = ActivityBusiness.find_by(supplier_id: current_supplier)
-    @activities = @activity_business.activities
-    @booked = BookedActivity.where(supplier_id: @supplier.id)
-    # 欲しいデータ
-    # [["日付", 合計予約金額],[],[]]
-    @booked6daysBefore = @booked.select { |b6| b6.purchase_date.to_date == (Date.today - 6) }
-    @booked5daysBefore = @booked.select { |b5| b5.purchase_date.to_date == (Date.today - 5) }
-    @booked4daysBefore = @booked.select { |b4| b4.purchase_date.to_date == (Date.today - 4) }
-    @booked3daysBefore = @booked.select { |b3| b3.purchase_date.to_date == (Date.today - 3) }
-    @booked2daysBefore = @booked.select { |b2| b2.purchase_date.to_date == (Date.today - 2) }
-    @booked1dayBefore = @booked.select { |b1| b1.purchase_date.to_date == (Date.today - 1) }
-    @bookedTodayBefore = @booked.select { |b0| b0.purchase_date.to_date == Date.today }
-    @data = [
-      [(Date.today - 6).strftime("%m/%d"),@booked6daysBefore.sum { |hash| hash[:total_price]}],
-      [(Date.today - 5).strftime("%m/%d"),@booked5daysBefore.sum { |hash| hash[:total_price]}],
-      [(Date.today - 4).strftime("%m/%d"),@booked4daysBefore.sum { |hash| hash[:total_price]}],
-      [(Date.today - 3).strftime("%m/%d"),@booked3daysBefore.sum { |hash| hash[:total_price]}],
-      [(Date.today - 2).strftime("%m/%d"),@booked2daysBefore.sum { |hash| hash[:total_price]}],
-      [(Date.today - 1).strftime("%m/%d"),@booked1dayBefore.sum { |hash| hash[:total_price]}],
-      [Date.today.strftime("%m/%d"),@bookedTodayBefore.sum { |hash| hash[:total_price]}]
-    ]
+    if @activity_business
+      @activities = @activity_business.activities
+      @booked = BookedActivity.where(supplier_id: @supplier.id)
+      # 欲しいデータ
+      # [["日付", 合計予約金額],[],[]]
+      @booked6daysBefore = @booked.select { |b6| b6.purchase_date.to_date == (Date.today - 6) }
+      @booked5daysBefore = @booked.select { |b5| b5.purchase_date.to_date == (Date.today - 5) }
+      @booked4daysBefore = @booked.select { |b4| b4.purchase_date.to_date == (Date.today - 4) }
+      @booked3daysBefore = @booked.select { |b3| b3.purchase_date.to_date == (Date.today - 3) }
+      @booked2daysBefore = @booked.select { |b2| b2.purchase_date.to_date == (Date.today - 2) }
+      @booked1dayBefore = @booked.select { |b1| b1.purchase_date.to_date == (Date.today - 1) }
+      @bookedTodayBefore = @booked.select { |b0| b0.purchase_date.to_date == Date.today }
+      @data = [
+        [(Date.today - 6).strftime("%m/%d"),@booked6daysBefore.sum { |hash| hash[:total_price]}],
+        [(Date.today - 5).strftime("%m/%d"),@booked5daysBefore.sum { |hash| hash[:total_price]}],
+        [(Date.today - 4).strftime("%m/%d"),@booked4daysBefore.sum { |hash| hash[:total_price]}],
+        [(Date.today - 3).strftime("%m/%d"),@booked3daysBefore.sum { |hash| hash[:total_price]}],
+        [(Date.today - 2).strftime("%m/%d"),@booked2daysBefore.sum { |hash| hash[:total_price]}],
+        [(Date.today - 1).strftime("%m/%d"),@booked1dayBefore.sum { |hash| hash[:total_price]}],
+        [Date.today.strftime("%m/%d"),@bookedTodayBefore.sum { |hash| hash[:total_price]}]
+      ]
+    else
+      redirect_to supplier_path(current_supplier.id)
+    end
   end
 
 
